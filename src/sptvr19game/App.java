96e17e063e008e72b566f827cbc075248bfca592
Zahhar <Zahhar.Pertsev@ivkhk.ee>
2020-09-16 10:40:36 +0300

    Готовый Проект

--- src/sptvr19game/App.java
+++ src/sptvr19game/App.java
@@ -5,6 +5,9 @@
  */
 package sptvr19game;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  *
  * @author user
@@ -12,6 +15,28 @@ package sptvr19game;
 class App {
     public void run(){
         System.out.println("Угадай задуманное число");
-        
+        System.out.println("--------------------------");
+        Random random = new Random();
+        int myNumber = random.nextInt(5-0+1)+0;
+        System.out.println("Задумано число от 0 до 5. Угадай!");
+        Scanner scanner = new Scanner(System.in);
+        int attempt = 1;
+        do{
+        int gamerNumber = scanner.nextInt();
+        if(myNumber == gamerNumber) {
+            System.out.println("Ты выиграл!");
+            break;
+        }else{
+            if (attempt < 3){
+                System.out.println("Не угадал, попробуй еще: ");
+            }else {
+            System.out.println("Ты проиграл. Задумано число: "+myNumber);
+        }
     }
+} while (true);
 }
+}
+  
+        
+    
+
--- src/sptvr19game/SPTVR19Game.java
+++ src/sptvr19game/SPTVR19Game.java
@@ -16,7 +16,7 @@ public class SPTVR19Game {
      */
     public static void main(String[] args) {
         App app = new App();
-        app.run
+        app.run(); 
     }
     
 }
